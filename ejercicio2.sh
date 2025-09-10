a7d07d7c48e4ef615d71e06a1f893cc4238fb974
AndresZumbadoMoreira <AndresZumbadoMoreira@outlook.com>
2025-09-10 22:51:14 +0000

    Se agregaron comentarios en la ultima seccion

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -21,21 +21,31 @@ while pgrep -x "$proceso" > /dev/null; do
     echo "$hora $consumo" | tee -a consumo.log
     sleep 1
 done
+#Para graficar el consumo se crearan dos graficas separadas#una para CPU y otra para RAM. 
+#Se utiliza <<EOF para excribir con la sintaxis de gnuplot a pesar de que el script es .sh
 
+# Grafica CPU
 gnuplot <<EOF
 set datafile separator whitespace
-
+#Se especifica que el formato es de tiempo
 set xdata time
+#Se especifica el formato del tiempo
 set timefmt "%H:%M:%S"
 set format x "%H:%M:%S"
+#Aqui se especifica el size y se utiliza cairo para que
+#las imagenes sean mas nitidas
 set terminal pngcairo size 1000,600
+#Se nombra el archivo, grafico y titulo de los ejes
 set output "cpu.png"
 set title "Consumo de CPU vs Tiempo"
 set xlabel "Hora"
 set ylabel "CPU (%)"
+#FInalmente se crea el grafico
 plot "consumo.log" using 1:2 with lines title "CPU"
 EOF
 
+#Ahora se ejecuta el mismo proceso pero para la grafica de la RAM
+#
 # Grafica RAM
 gnuplot <<EOF
 set datafile separator whitespace
