a885c74cdb36a1c8671117e4a4f17444bd802281
AndresZumbado <AndresZumbadoMoreira@outlook.com>
2025-09-09 18:13:36 -0600

    Se modifico el archivo para que verifique si el proceso existe antes de ejecutarlo. Tambien se agrego la funcion de ejecutar el proceso y que devuelva el consumo de cpu y memoria cada segundo en la terminal.

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-proceso=$1
-
-if [ -z proceso ]; then
+proceso=$@
+if [ -z "$proceso" ]; then
 	echo "Debe introducir un proceso a agregar"
 	exit 1
 fi
 
-$proceso
+if [ -z "$(command -v $proceso)" ]; then
+	echo "El proceso no existe"
+	exit 2
+fi
+
+$proceso &
+PID=$!
+
+while kill -0 $PID 2>/dev/null; do
+    ps -p "$PID" -o %cpu,%mem --no-headers
+    sleep 1
+done
+
+
