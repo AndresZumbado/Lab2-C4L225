6bc26da0dd8342d4fe6ce66c3f441297e8de1ad2
AndresZumbado <AndresZumbadoMoreira@outlook.com>
2025-09-10 05:47:55 -0600

    Ahora la salida contiene una nueva columna con la fecha en que se hizo cada lectura

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-proceso=$@
+proceso=$1
 if [ -z "$proceso" ]; then
 	echo "Debe introducir un proceso a agregar"
 	exit 1
@@ -14,9 +14,10 @@ fi
 $proceso &
 PID=$!
 
-while kill -0 $PID 2>/dev/null; do
-    ps -p "$PID" -o %cpu,%mem --no-headers | tee -a consumo.log
+while kill -0 "$PID" 2>/dev/null; do
+    hora=$(date +"%H:%M:%S")
+    consumo=$(ps -p "$PID" -o %cpu,%mem --no-headers)
+    echo "$hora $consumo" | tee -a consumo.log
     sleep 1
 done
-
 
