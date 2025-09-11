05563cc6a6df533013184fafbf8c4c9b0a291098
AndresZumbado <AndresZumbadoMoreira@outlook.com>
2025-09-11 14:57:29 -0600

    Se agregaron comentarios

--- ejercicio3.sh
+++ ejercicio3.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+#Directorio y ruta de archivo escogidos arbitrariamente en mi maquina
 directorio="/home/andresvirtual/Downloads"
 archivo_log="/home/andresvirtual/Documents/cambios.log"
 
+#Se asegura que el archivo exista y este vacio
 > "$archivo_log"
+#Se activa el inotifywait
+#-m activa el modo de monitoreo
+#-r es el modo de recursividad que permite ver cambios en subdirectorios
+#--format expecifica el formato
 inotifywait -m -r -e create -e modify -e delete "$directorio" --format '%w%f %e' |
 while read archivo evento
 do
+	#Se guarda en el cambios.log con la fecha y hora del evento
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $evento en $archivo" >> "$archivo_log"
 done
 
